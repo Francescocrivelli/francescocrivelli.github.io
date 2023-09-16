87301bf3169261aba800f5646a5b3f576b21411f
Francesco Crivelli <crivelli@berkeley.edu>
2023-09-15 19:51:11 -0700

    website v1

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -8,6 +8,7 @@ const endpoints = {
   education: 'profile/education.json',
   experiences: 'profile/experiences.json',
   projects: 'profile/projects.json',
+  loopline: 'profile/loopline.json',
 };
 
 export default endpoints;
